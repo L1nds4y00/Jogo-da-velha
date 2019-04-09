a4e50d5aca03a265a15e653688425b6249c4bd7c
Lara Lindsay <48735715+L1nds4y00@users.noreply.github.com>
2019-04-09 11:50:16 -0300

    Add files via upload

---
+++ Jogo da Velha/JS/arquivo.js
@@ -0,0 +1,13 @@
+const jogador01 = "X";
+const jogador02 = "O";
+var vezDe = jogador01;
+var gameOver = false;
+
+
+resultado ();
+ if (gameOver) {return}
+
+ if (playtime == jogador01) {
+     var jogador = document.querySelectorAll
+ }
+ 
