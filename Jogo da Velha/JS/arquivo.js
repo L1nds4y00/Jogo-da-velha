dbac7871f11a2104e095c0e4e8f2fd365c10df4c
Lara Lindsay <48735715+L1nds4y00@users.noreply.github.com>
2019-04-10 12:02:29 -0300

    Add files via upload

--- Jogo da Velha/JS/arquivo.js
+++ Jogo da Velha/JS/arquivo.js
@@ -1,13 +1,18 @@
-const jogador01 = "X";
-const jogador02 = "O";
-var vezDe = jogador01;
-var gameOver = false;
+var jogador1 = 'img/death-star.png';
+var jogador2 = 'img/lightsaber.png';
+var velha = 'img/palpatine.png';
+var jogada = true;
 
 
-resultado ();
- if (gameOver) {return}
 
- if (playtime == jogador01) {
-     var jogador = document.querySelectorAll
- }
- 
+
+function Clicar(area) {
+    if (jogada) {
+        area.innerHTML = `<img src= "${jogador1}" style= " width: 140px" "height: 50px" />`;
+        jogada = false;
+    } else if (!jogada) {
+        area.innerHTML = `<img src= "${jogador2}" style= " width: 140px" "height: 8 0px" />`;
+        jogada = true;
+
+    }
+}
